431212c504f22d01acf2a3923dbae88b5abe9b7e
work <work@localhost>
2025-06-24 17:42:38 -0400

    Initial Commit

---
+++ springframework/src/test/java/com/spring_projects/springframework/SpringframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_projects.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
