6d864423e4c6fa1fea8fd812e7e3daf73df8cead
work <work@localhost>
2025-06-24 18:28:26 -0400

    Pacman Game

--- springframework/src/main/java/com/spring_projects/springframework/AppGamingBasicJava.java
+++ springframework/src/main/java/com/spring_projects/springframework/AppGamingBasicJava.java
@@ -2,6 +2,7 @@ package com.spring_projects.springframework;
 
 import com.spring_projects.springframework.game.GameRunner;
 import com.spring_projects.springframework.game.MarioGame;
+import com.spring_projects.springframework.game.PacManGame;
 import com.spring_projects.springframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
@@ -10,10 +11,13 @@ public class AppGamingBasicJava {
 		// TODO Auto-generated method stub
 		var game1 = new MarioGame();
 		var game2 = new SuperContraGame();
+		var game3 = new PacManGame();
 		var gameRunner = new GameRunner(game1);
 		var gameRunner2 = new GameRunner(game2);
+		var gameRunner3 = new GameRunner(game3);
 		gameRunner.run();
 		gameRunner2.run();
+		gameRunner3.run();
 		
 		
 	}
---
+++ springframework/src/main/java/com/spring_projects/springframework/game/PacManGame.java
@@ -0,0 +1,17 @@
+package com.spring_projects.springframework.game;
+
+public class PacManGame implements GamingConsole {
+	public void up() {
+		System.out.println("PacMan UP");
+	}
+	public void down() {
+		System.out.println("PacMan DOWN");
+	}
+	public void left() {
+		System.out.println("PacMan LEFT");
+	}
+	public void right() {
+		System.out.println("PacMan RIGHT");
+	}
+
+}
